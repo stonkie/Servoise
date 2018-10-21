625e923178d6f1b2ce8cc1818b594b771ab15a85
Stonkie <stonkie@gmail.com>
2018-10-21 12:43:54 -0400

    ADD - Solution template

---
+++ Byteauthor.Servoise.Web/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Byteauthor.Servoise.Web.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
